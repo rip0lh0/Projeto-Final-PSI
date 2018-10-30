aaa3c69a804d26a3a2cf58f70788606eb0492d46
Francisco Fernandes <fandreliraf@gmail.com>
2018-10-30 14:11:44 +0000

    #Fontawesome #Backend #Login
    - Add Fontawesome
    - Add kartik Extention (Form Groups)

--- webapp/backend/views/site/login.php
+++ webapp/backend/views/site/login.php
@@ -5,7 +5,7 @@
 /* @var $model \common\models\LoginForm */
 
 use yii\helpers\Html;
-use yii\bootstrap\ActiveForm;
+use kartik\form\ActiveForm;
 use backend\assets\LoginAsset;
 
 LoginAsset::register($this);
@@ -13,19 +13,28 @@ LoginAsset::register($this);
 $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="col-md-6 col-md-offset-3">
+<div class="col-md-4 col-md-offset-4">
     <div class="panel panel-login">
         <div class="panel-login-heading">
             <h3 class="panel-login-title"><?= Html::encode($this->title) ?></h3>
         </div>
         <div class="panel-body">
-            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
-                <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
-                <?= $form->field($model, 'password')->passwordInput() ?>
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
-                <div class="form-group">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-                </div>
+            <?php $form = ActiveForm::begin([
+                'id' => 'login-form',
+                'type' => ActiveForm::TYPE_VERTICAL
+            ]); ?>
+                <!-- Username Input -->
+                <?= $form->field($model, 'username', ['addon' => ['prepend' => ['content' => '<i class="fa fa-user"></i>']]])->textInput([
+                    'autofocus' => true,
+                    'placeholder' => 'Username',
+                    'class' => ''
+                ])->label(false); ?>
+                <!-- Password Input -->
+                <?= $form->field($model, 'password', ['addon' => ['prepend' => ['content' => '<i class="fa fa-lock"></i>']]])->passwordInput([
+                    'placeholder' => 'Password'
+                ])->label(false); ?>
+
+                <?= Html::submitButton('Login', ['class' => 'btn btn-default btn-lg btn-block', 'name' => 'login-button']) ?>
             <?php ActiveForm::end(); ?>
         </div>
     </div>
