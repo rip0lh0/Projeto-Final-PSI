1ef47f63d92b346e5bc0aafd02d7518debf59fcf
Matheus De Oliveira Ferreira <2170372@my.ipleiria.pt>
2018-11-21 22:39:02 +0000

    #Android#Layout#Presentations
    Added Presentations folder and Presentation01

---
+++ Android/Pet4All/app/src/main/java/pt/amsi/ipleiria/pet4all/SettingsFragment.java
@@ -0,0 +1,44 @@
+package pt.amsi.ipleiria.pet4all;
+
+import android.content.Context;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+
+public class SettingsFragment extends Fragment {
+    Boolean settingsState=false;    // True = Open : False = Close
+    ImageButton btnSettings,btnReport,btnEditProfile;
+
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
+
+
+    // TODO: Rename and change types and number of parameters
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+
+    }
+}
+
