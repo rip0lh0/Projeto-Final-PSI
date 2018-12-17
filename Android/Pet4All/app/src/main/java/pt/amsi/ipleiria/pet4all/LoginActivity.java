6252a0fff41eede8c5e3784f6f2d959caa1d0ff3
Matheus De Oliveira Ferreira <2170372@my.ipleiria.pt>
2018-12-17 17:55:56 +0000

    #Android #Bugs
    Bug - can't access RegisterKennelActivity or RegisterUserActivity from RegisterMenuActivity

--- Android/Pet4All/app/src/main/java/pt/amsi/ipleiria/pet4all/LoginActivity.java
+++ Android/Pet4All/app/src/main/java/pt/amsi/ipleiria/pet4all/LoginActivity.java
@@ -3,6 +3,7 @@ package pt.amsi.ipleiria.pet4all;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -277,6 +278,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                         android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
 
         //mEmailView.setAdapter(adapter);
+    }
+
+    public void btnRegisterRedirect(View view) {
+        Intent intent= new Intent(this,RegisterMenuActivity.class);
+        startActivity(intent);
     }
 
     private interface ProfileQuery {
