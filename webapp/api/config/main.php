d543107daedadb5381dc9af1ac6bca13a8ffe33f
Francisco Fernandes <fandreliraf@gmail.com>
2018-12-20 22:28:19 +0000

    #Revert

---
+++ webapp/api/config/main.php
@@ -0,0 +1,57 @@
+<?php
+$params = array_merge(
+    require __DIR__ . '/../../common/config/params.php',
+    require __DIR__ . '/../../common/config/params-local.php',
+    require __DIR__ . '/params.php',
+    require __DIR__ . '/params-local.php'
+);
+
+return [
+    'id' => 'app-api',
+    'name' => 'Pet4all',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'controllerNamespace' => 'api\controllers',
+    'components' => [
+        'request' => [
+            'csrfParam' => '_csrf-api',
+            'cookieValidationKey' => 'Xwf3Djk4sS2IqqGlhiUBQDbp_rbYhtcp',
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser'
+            ]
+        ],
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => false,
+            'identityCookie' => ['name' => '_identity-api', 'httpOnly' => true],
+        ],
+        'session' => [
+            // this is the name of the session cookie used for login on the api
+            'name' => 'api',
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+                [
+                    'class' => 'yii\rest\UrlRule',
+                    'controller' => 'api\site',
+                    'pluralize' => false
+                ]
+            ]
+        ]
+    ],
+    'params' => $params,
+];
