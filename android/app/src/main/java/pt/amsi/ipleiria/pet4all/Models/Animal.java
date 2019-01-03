d938e19ad6d8a31a6e393c3dfeb22d08100d00b0
Francisco Fernandes <fandreliraf@gmail.com>
2019-01-03 15:20:25 +0000

    #Models
    - Animal

--- android/app/src/main/java/pt/amsi/ipleiria/pet4all/Models/Animal.java
+++ android/app/src/main/java/pt/amsi/ipleiria/pet4all/Models/Animal.java
@@ -1,4 +1,46 @@
 package pt.amsi.ipleiria.pet4all.Models;
 
 public class Animal {
+    public final static String DB_NAME = "animal";
+
+    private int id;
+    private String name;
+    private String description;
+
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getDescription() {
+        return description;
+    }
+
+    public Animal(){}
+    public Animal(String name, String description){
+        this.name = name;
+        this.description = description;
+    }
+
+    public void update(String name, String description){
+        this.name = name;
+        this.description = description;
+    }
+
+    public void update(String name){
+        this.update(name, this.description);
+    }
+
+    //public void update(String description){
+    //    this.update(this.name, this.description);
+    //}
+
+    public static void delete(Animal animal){
+
+    }
+
+    public int generateID(){
+        return 0;
+    }
 }
