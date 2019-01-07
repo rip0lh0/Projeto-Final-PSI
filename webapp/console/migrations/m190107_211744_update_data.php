bc7073b2421364d056a680391e44e96bc756cd41
Pacheco <2170377@my.ipleiria.pt>
2019-01-07 22:40:42 +0000

    #Migrations
    - Ajusts

--- webapp/console/migrations/m190107_211744_update_data.php
+++ webapp/console/migrations/m190107_211744_update_data.php
@@ -15,79 +15,81 @@ class m190107_211744_update_data extends Migration
         $tableOptions = null;
         if ($this->db->driverName === 'mysql') $tableOptions = 'CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE=InnoDB';
 
-        /* Animal Table */
-        $this->addColumn('animal', 'id_coat', $this->integer());
-        $this->addColumn('animal', 'id_energy', $this->integer());
-        $this->addColumn('animal', 'id_size', $this->integer());
-        $this->addColumn('animal', 'chip', $this->string()->unique());
-        $this->addColumn('animal', 'age', $this->float()->null());
-        $this->addColumn('animal', 'gender', $this->integer());
-        $this->addColumn('animal', 'weight', $this->integer());
-        $this->addColumn('animal', 'neutered', $this->tinyInteger()->notNull());
-        $this->addColumn('animal', 'created_at', $this->dateTime()->notNull());
-        $this->addColumn('animal', 'updated_at', $this->dateTime()->notNull());
-        $this->addColumn('animal', 'status', $this->integer()->notNull());
+        // /* Animal Table */
+        // $this->addColumn('animal', 'id_coat', $this->integer());
+        // $this->addColumn('animal', 'id_energy', $this->integer());
+        // $this->addColumn('animal', 'id_size', $this->integer());
+        // $this->addColumn('animal', 'chip', $this->string()->unique());
+        // $this->addColumn('animal', 'age', $this->float()->null());
+        // $this->addColumn('animal', 'gender', $this->integer());
+        // $this->addColumn('animal', 'weight', $this->integer());
+        // $this->addColumn('animal', 'neutered', $this->tinyInteger()->notNull());
+        // $this->addColumn('animal', 'created_at', $this->dateTime()->notNull());
+        // $this->addColumn('animal', 'updated_at', $this->dateTime()->notNull());
+        // $this->addColumn('animal', 'status', $this->integer()->notNull());
 
-        /* Drop Foreign Key & Index */
-        $this->dropForeignKey(
-            'fk-treatment-id_animal_file',
-            '{{%treatment}}'
-        );
+        // /* Drop Foreign Key & Index */
+        // $this->dropForeignKey(
+        //     'fk-treatment-id_animal_file',
+        //     '{{%treatment}}'
+        // );
 
-        $this->dropIndex(
-            'idx-treatment-id_animal_file',
-            '{{%treatment}}'
-        );
+        // $this->dropIndex(
+        //     'idx-treatment-id_animal_file',
+        //     '{{%treatment}}'
+        // );
 
-        $this->dropForeignKey(
-            'fk-animal_file-id_animal',
-            '{{%animal_file}}'
-        );
+        // $this->dropForeignKey(
+        //     'fk-animal_file-id_animal',
+        //     '{{%animal_file}}'
+        // );
 
-        $this->dropIndex(
-            'idx-animal_file-id_animal',
-            '{{%animal_file}}'
-        );
+        // $this->dropIndex(
+        //     'idx-animal_file-id_animal',
+        //     '{{%animal_file}}'
+        // );
 
-        $this->dropForeignKey(
-            'fk-user-id_local',
-            '{{%user}}'
-        );
+        // $this->dropForeignKey(
+        //     'fk-user-id_local',
+        //     '{{%user}}'
+        // );
 
-        $this->dropIndex(
-            'idx-user-id_local',
-            '{{%user}}'
-        );
+        // $this->dropIndex(
+        //     'idx-user-id_local',
+        //     '{{%user}}'
+        // );
 
-        $this->dropForeignKey(
-            'fk-kennel-id_user',
-            '{{%kennel}}'
-        );
+        // $this->dropForeignKey(
+        //     'fk-kennel-id_user',
+        //     '{{%kennel}}'
+        // );
 
-        $this->dropIndex(
-            'idx-kennel-id_user',
-            '{{%kennel}}'
-        );
+        // $this->dropIndex(
+        //     'idx-kennel-id_user',
+        //     '{{%kennel}}'
+        // );
 
-        $this->dropForeignKey(
-            'fk-kennel-id_contact',
-            '{{%kennel}}'
-        );
+        // $this->dropForeignKey(
+        //     'fk-kennel-id_contact',
+        //     '{{%kennel}}'
+        // );
 
-        $this->dropIndex(
-            'idx-kennel-id_contact',
-            '{{%kennel}}'
-        );
+        // $this->dropIndex(
+        //     'idx-kennel-id_contact',
+        //     '{{%kennel}}'
+        // );
 
-        $this->dropForeignKey(
-            'fk-kennel-id_social',
-            '{{%kennel}}'
-        );
+        // $this->dropForeignKey(
+        //     'fk-kennel-id_social',
+        //     '{{%kennel}}'
+        // );
 
-        $this->dropIndex(
-            'idx-kennel-id_social',
-            '{{%kennel}}'
-        );
+        // $this->dropIndex(
+        //     'idx-kennel-id_social',
+        //     '{{%kennel}}'
+        // );
+
+        $this->addColumn('treatment', 'id_animal', $this->integer()->notNull());
 
         $this->createIndex(
             'idx-treatment-id_animal',
@@ -141,7 +143,6 @@ class m190107_211744_update_data extends Migration
         );
 
         /* New Columns */
-        $this->addColumn('treatment', 'id_animal');
         $this->addColumn('kennel', 'id_local', $this->integer()->notNull());
         $this->addColumn('kennel', 'phone', $this->string());
         $this->addColumn('kennel', 'cell_phone', $this->string());
